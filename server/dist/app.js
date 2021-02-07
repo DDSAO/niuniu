0248399d31ea0e201d9854cc969a21eac4d4c340
Hao Sun <hao.sun@uq.net.au>
2021-02-07 14:22:07 +1000

    cleared server

--- server/dist/app.js
+++
@@ -1,8 +0,0 @@
-/******/ (() => { // webpackBootstrap
-/*!****************!*\
-  !*** ./app.js ***!
-  \****************/
-console.log("hello world")
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9zZXJ2ZXIvLi9hcHAuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OztBQUFBLDBCIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiaGVsbG8gd29ybGRcIikiXSwic291cmNlUm9vdCI6IiJ9
